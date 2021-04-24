d3853ee5065f8dea3e8d78946dc042d2e37fd4a4
Thanh Trung <thanhtrung20022001@gmail.com>
2021-04-25 00:42:22 +0700

    Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,7 +8,27 @@ const Services = () =>{
                 <img id="img-services" src="https://themebeyond.com/html/movflx/img/images/services_img.jpg" alt=""/>
             </div>
             <div className="services__right">
-                aaa
+                <span className="services__right--title">
+                    <span className="services__right--title--content">OUR SERVICES</span> 
+                </span>
+                <br/>
+                <h2>Download Your Shows <br/> Watch Offline.</h2>
+                <p>Lorem ipsum dolor sit amet, consecetur adipiscing elseddo 
+                    eiusmod tempor.There are many variations of passages of 
+                    lorem Ipsum available, but the majority have suffered 
+                    alteration in some injected humour.
+                </p>
+                <div className="services__right--circles">
+                    <div className="circles">
+
+                    </div>
+                </div>
+                <hr className="hr"/>
+                <div className="services__right--circles">
+                    <div className="circles">
+
+                    </div>
+                </div>
             </div>
         </div>
     )
