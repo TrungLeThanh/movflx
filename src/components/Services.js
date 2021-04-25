dccf5b3351e2773166079517d206add847964a7d
Thanh Trung <thanhtrung20022001@gmail.com>
2021-04-25 17:29:33 +0700

    services done!

--- src/components/Services.js
+++ src/components/Services.js
@@ -13,20 +13,28 @@ const Services = () =>{
                 </span>
                 <br/>
                 <h2>Download Your Shows <br/> Watch Offline.</h2>
-                <p>Lorem ipsum dolor sit amet, consecetur adipiscing elseddo 
+                <p id="none">Lorem ipsum dolor sit amet, consecetur adipiscing elseddo 
                     eiusmod tempor.There are many variations of passages of 
                     lorem Ipsum available, but the majority have suffered 
                     alteration in some injected humour.
                 </p>
                 <div className="services__right--circles">
                     <div className="circles">
-
+                        <i style={{position: 'absolute', color: '#fff', fontSize: '38px', top: '24px', left: '18px'}} className="fas fa-tv" />
+                    </div>
+                    <div className="circles--content">
+                        <h5>Enjoy on Your TV.</h5>
+                        <p >Lorem ipsum dolor sit amet, consecetur adipiscing <br /> elit, sed do eiusmod tempor.</p>
                     </div>
                 </div>
                 <hr className="hr"/>
                 <div className="services__right--circles">
                     <div className="circles">
-
+                        <i style={{position: 'absolute', color: '#fff', fontSize: '38px', top: '24px', left: '18px'}} className="fas fa-video" />
+                    </div>
+                    <div className="circles--content">
+                        <h5>Watch Everywhere.</h5>
+                        <p>Lorem ipsum dolor sit amet, consecetur adipiscing <br /> elit, sed do eiusmod tempor.</p>
                     </div>
                 </div>
             </div>
